019c9b96672e91a4c3f6196e145b6bbd85da1a14
SergioAC52 <sergio.antona1@educa.madrid.org>
2023-03-19 13:19:21 +0100

    Añadido método mediaArray y su ejemplo en el main.

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -1,10 +1,21 @@
 package main.java.ieseuropa;
 
 public class Array {
-
+	private static int mediaArray(int[] array){
+		int sumatorio = 0;
+		for (int i = 0; i < array.length; i++) {
+			sumatorio += array[i];
+		}
+		return sumatorio/array.length;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
+		
+		//Media
+		int[] array = {8,2,3,4,4};
+		System.out.println(mediaArray(array));
 	}
 
 }
