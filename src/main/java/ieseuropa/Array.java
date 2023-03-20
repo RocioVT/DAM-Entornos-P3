eddd261a03c0dd3aae1aba635c5b62a241a5c8fa
AuroCR97 <auroconderello@gmail.com>
2023-03-20 16:44:32 +0100

    Tarea max array

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -1,10 +1,20 @@
 package main.java.ieseuropa;
 
 public class Array {
+	
+	private static int calcularMax(int[] numeros) {
+		int mayor = 0;
+		for (int i = 1; i < numeros.length; i++) {
+			if (numeros[i] > mayor) {
+				mayor = numeros[i];
+			}
+		}
+		return mayor;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola");
+
 	}
 
 }
