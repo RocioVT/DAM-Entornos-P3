347e454c86b6eb69a3fd0ec95307ed17d4981530
AuroCR97 <auroconderello@gmail.com>
2023-03-20 15:16:15 +0100

    Tarea factorial completada

---
+++ src/main/java/ieseuropa/Numero.java
@@ -0,0 +1,15 @@
+package main.java.ieseuropa;
+
+public class Numero {
+
+	public static int calcularFactorial(int num) {
+		if (num < 0) {
+			System.out.println("No se puede hacer el factorial de un negativo");
+		}
+		int factorial = 1;
+		for (int i = 2; i <= num; i++) {
+			factorial *= i;
+		}
+		return factorial;
+	}
+}
