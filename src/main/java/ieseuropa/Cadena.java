851f7efa4e2a5ae11a70a9e649a18178a21cbb43
AuroCR97 <auroconderello@gmail.com>
2023-03-20 13:52:17 +0100

    Tarea longitud completada

---
+++ src/main/java/ieseuropa/Cadena.java
@@ -0,0 +1,10 @@
+package main.java.ieseuropa;
+
+public class Cadena {
+
+	private static int calcularLongitud (String texto) {
+		String textoModificado = texto.replace(" ", "");
+		return textoModificado.length();
+	}
+	
+}
