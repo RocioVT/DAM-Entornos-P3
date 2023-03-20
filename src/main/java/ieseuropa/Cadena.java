eb1b146382bc0e5d305fa6ebdb28ee50ce74a1e1
AuroCR97 <auroconderello@gmail.com>
2023-03-20 14:07:05 +0100

    Tarea completada

---
+++ src/main/java/ieseuropa/Cadena.java
@@ -0,0 +1,13 @@
+package main.java.ieseuropa;
+
+public class Cadena {
+	
+	private static String cambiarMayusc(String texto) {
+		return texto.toUpperCase();
+	}
+	
+	private static String cambiarMinusc(String texto) {
+		return texto.toLowerCase();
+	}
+	
+}
