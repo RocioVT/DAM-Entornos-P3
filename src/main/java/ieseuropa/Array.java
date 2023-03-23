ee582eb3031c15b396ec6df58aa993dd9d242da8
SergioAC52 <sergio.antona1@educa.madrid.org>
2023-03-24 00:39:47 +0100

    Corregir mediana

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -1,5 +1,7 @@
 package main.java.ieseuropa;
 
+import java.util.Arrays;
+
 public class Array {
 	private static int mediaArray(int[] array){
 		int sumatorio = 0;
@@ -10,6 +12,7 @@ public class Array {
 	}
 	
 	private static int medianaArray(int[] array) {
+		Arrays.sort(array);
 		int numeroCentral = array.length/2;
 		if(array.length % 2 != 0) {
 			return array[numeroCentral];
@@ -24,9 +27,9 @@ public class Array {
 		
 		//Media
 		int[] array = {8,2,3,4,4};
-		System.out.println(mediaArray(array));
+		System.out.println("Media: "+mediaArray(array));
 		//Mediana
-		System.out.println(medianaArray(array));
+		System.out.println("Mediana: "+medianaArray(array));
 	}
 
 }
