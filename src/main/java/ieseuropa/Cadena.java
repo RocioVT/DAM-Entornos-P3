9baf15e728f0acc54072144bae5291abdf4dea16
AuroCR97 <auroconderello@gmail.com>
2023-03-23 19:33:58 +0100

    Con ejemplo

--- src/main/java/ieseuropa/Cadena.java
+++ src/main/java/ieseuropa/Cadena.java
@@ -1,7 +1,7 @@
 package main.java.ieseuropa;
 
 public class Cadena {
-	
+
 	private static int calcularOcurrenciasLetra(String texto, String letra) {
 		int contOcurrencias = 0;
 		char letraChar = letra.toLowerCase().charAt(0);
@@ -11,5 +11,13 @@ public class Cadena {
 			}
 		}
 		return contOcurrencias;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String texto = "Mari Carmen";
+		String letra = "a";
+		System.out.println(calcularOcurrenciasLetra(texto, letra));
 	}
 }
