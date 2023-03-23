0657e3d251e52778808892b10e758dbafb05186e
SergioAC52 <sergio.antona1@educa.madrid.org>
2023-03-24 00:56:25 +0100

    Implementado redondear al alza

--- src/main/java/ieseuropa/Decimales.java
+++ src/main/java/ieseuropa/Decimales.java
@@ -5,9 +5,13 @@ public class Decimales {
 	private static int eliminarDecimales(float num){
 		return (int)(num);
 	}
-	
+	private static int redondearAlza(float num) {
+		return (int) Math.ceil(num);
+	}
 	public static void main(String[] args) {
 		
-		System.out.println("Eliminar decimales: "+eliminarDecimales(25.66f));	}
-
+		System.out.println("Eliminar decimales: "+eliminarDecimales(25.66f));
+		System.out.println("Redondear al alza: "+redondearAlza(15.2f));
+	}
+		
 }
