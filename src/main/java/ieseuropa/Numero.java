41dae7b0165d8edfbd6062af58746dcfaa0aa6e0
AuroCR97 <auroconderello@gmail.com>
2023-03-23 20:07:32 +0100

    Con ejemplo

--- src/main/java/ieseuropa/Numero.java
+++ src/main/java/ieseuropa/Numero.java
@@ -12,4 +12,10 @@ public class Numero {
 		}
 		return factorial;
 	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		System.out.println(calcularFactorial(5));
+	}
 }
