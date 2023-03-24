a20abcd3e6e8d34548cfbc0de7fe94506196fd5a
AitorSV <aitorsuarez.vic@gmail.com>
2023-03-24 20:03:12 +0100

    metodo repetir letras

--- src/main/java/ieseuropa/Cadena.java
+++ src/main/java/ieseuropa/Cadena.java
@@ -11,11 +11,23 @@ public class Cadena {
 		
 		return cadenaRep;
 	}
+	private static String repetirLetras(String texto) {
+		String repetido="";
+		int repeticiones=2;
+		for(int i=0;i<texto.length();i++) {
+			for(int j=0;j<repeticiones;j++) {
+				repetido+=texto.charAt(i);	
+			}
+		}
+		
+		return repetido;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		
 		System.out.println(repetirStrings("Hola", "Hamijo",3));
+		System.out.println(repetirLetras("Rock&Stone4ever"));
 	}
 
 }
